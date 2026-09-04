27b3b4801ace8963563bb9a680a981cce9558b29
Efty34 <efty17.hasan@gmail.com>
2026-09-04 21:14:47 +0600

    backend folder strucutre adjsuted

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,34 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { AccountsModule } from './accounts/accounts.module';
+import { LedgerModule } from './ledger/ledger.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    // load from .env filen
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: '',
+      port: 5432,
+      username: '',
+      password: '',
+      database: '',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    AuthModule,
+    UsersModule,
+    AccountsModule,
+    LedgerModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
